4d5072c487b1b8eef7e8e95b5f4b6081647e8a8e
DanielLenoz <danielstivenxx@outlook.com>
2023-09-28 22:21:37 -0500

    base code

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://daniellenoz.github.io/fake-shoppi'
+})
